e7dd60f40c4d01adec55efcb60d5c848f5128aa5
sjhkr2009 <49022232+sjhkr2009@users.noreply.github.com>
2019-11-16 22:59:28 +0900

    week05

---
+++ Assets/2.Scripts/05/LerpStudy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpStudy : MonoBehaviour
+{
+    Vector3 target = new Vector3(0f, 2f, 0f);
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Vector3 lerpVector = Vector3.Lerp(Vector3.zero, target, 0.2f);
+        Debug.Log(lerpVector);
+        
+    }
+}
