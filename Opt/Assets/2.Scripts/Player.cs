7867e8f78e6a55933e4850243e9d727e57df5443
sjhkr2009 <49022232+sjhkr2009@users.noreply.github.com>
2019-11-30 07:22:12 +0900

    Create Opt

---
+++ Opt/Assets/2.Scripts/Player.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour, IUnit
+{
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    public StageManager stageManager;
+
+    public MoveUI moveUI;
+    public float speed = 3f;
+    public float originSpeed;
+    bool canMove;
+    public int hp;
+    public float noAttackedTime = 1f;
+
+    public enum State
+    {
+        Idle,Attacked,Destroyed,Clear
+    }
+
+    public State state
+    {
+        get
+        {
+            return playerState;
+        }
+        set
+        {
+            switch (value)
+            {
+                case State.Idle:
+                    playerState = value;
+                    StopCoroutine("AfterAttacked");
+                    break;
+                case State.Attacked:
+                    playerState = value;
+                    StartCoroutine("AfterAttacked");
+                    break;
+                case State.Destroyed:
+                    playerState = value;
+                    canMove = false;
+                    rb.velocity = Vector2.zero;
+                    stageManager.state = StageManager.State.GameOver;
+                    break;
+                case State.Clear:
+                    playerState = value;
+                    canMove = false;
+                    rb.velocity = Vector2.zero;
+                    stageManager.state = StageManager.State.StageClear;
+                    break;
+            }
+        }
+    }
+    private State playerState;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        canMove = true;
+        originSpeed = speed;
+    }
+
+    void Update()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+
+        rb.velocity = moveUI.PlayerMove() * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Goal")
+        {
+            state = State.Clear;
+        }
+    }
+
+    public void Attacked(int damage)
+    {
+        if (state == State.Idle)
+        {
+            hp -= damage;
+            state = State.Attacked;
+        }
+
+        if (hp <= 0 && state != State.Destroyed)
+        {
+            state = State.Destroyed;
+        }
+    }
+
+    IEnumerator AfterAttacked()
+    {
+        float cooltime = 0;
+        while (cooltime < noAttackedTime - 0.2f)
+        {
+            sr.color = new Color(1, 1, 1, 0.5f);
+            yield return new WaitForSeconds(0.05f);
+            sr.color = new Color(1, 0, 0, 0.8f);
+            yield return new WaitForSeconds(0.05f);
+            cooltime = cooltime + 0.1f;
+        }
+        sr.color = new Color(1, 1, 1, 1);
+        yield return new WaitForSeconds(0.2f);
+
+        if (state == State.Attacked)
+        {
+            state = State.Idle;
+        }
+    }
+}
---
+++ Opt/Assets/2.Scripts/StageManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    ScoreManager scoreManager;
+    LaserManager laserManager;
+    public GameObject launcher;
+    public Camera cam;
+    Vector3 clickPoint;
+
+    GameObject selectLauncherUI;
+    GameObject selectItemUI;
+    public bool isStageEnd;
+
+    public enum State
+    {
+        Idle,Hacking,ItemUsing,GameOver,StageClear
+    }
+    public State state
+    {
+        get
+        {
+            return stageState;
+        }
+        set
+        {
+            switch (value)
+            {
+                case State.Idle:
+                    stageState = value;
+                    selectLauncherUI.SetActive(false);
+                    selectItemUI.SetActive(false);
+                    break;
+                case State.Hacking:
+                    stageState = value;
+                    selectLauncherUI.SetActive(true);
+                    break;
+                case State.ItemUsing:
+                    stageState = value;
+                    selectItemUI.SetActive(true);
+                    laserManager.ItemUsing();
+                    break;
+                case State.GameOver:
+                    stageState = value;
+                    isStageEnd = true;
+                    Debug.Log("Game Over!"); //게임오버 창 출력
+                    break;
+                case State.StageClear:
+                    stageState = value;
+                    isStageEnd = true;
+                    scoreManager.GetScore();
+                    Debug.Log("Clear!"); //클리어 창 출력
+                    break;
+            }
+        }
+    }
+    private State stageState;
+
+    void Start()
+    {
+        selectLauncherUI = transform.Find("SelectLauncherUI").gameObject;
+        selectItemUI = transform.Find("SelectItemUI").gameObject;
+        laserManager = GetComponent<LaserManager>();
+        scoreManager = GetComponent<ScoreManager>();
+        state = State.Idle;
+        isStageEnd = false;
+    }
+
+
+    void Update()
+    {
+        if (isStageEnd)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(clickPoint, Vector2.zero);
+
+            switch (state)
+            {
+                case State.Idle:
+                    if (hit.collider == null)
+                    {
+                        Debug.Log("빈 공간");
+                        return;
+                    }
+
+                    switch (hit.collider.tag)
+                    {
+                        /*case "LaserLauncher":
+                            state = State.Hacking;
+                            laserManager.Hacking(hit.collider.gameObject);
+                            break;*/
+                        default:
+                            Debug.Log("할당된 동작 없음");
+                            break;
+                    }
+                    break;
+                    
+                case State.Hacking:
+                    state = State.Idle;
+                    if(hit.collider == null || hit.collider.tag != "LaserLauncher")
+                    {
+                        laserManager.HackingToIdle();
+                    }
+                    break;
+
+            }
+        }
+    }
+}
