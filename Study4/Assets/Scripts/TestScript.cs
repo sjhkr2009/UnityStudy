5fd1bf36f7a53554505897e32e4e14f1befb311d
sjhkr2009 <49022232+sjhkr2009@users.noreply.github.com>
2019-12-21 23:13:07 +0900

    9week

---
+++ Study4/Assets/Scripts/TestScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    TestObject testObject;
+    void Start()
+    {
+        testObject = Resources.Load<TestObject>("TestObject");
+    }
+
+    void Update()
+    {
+        
+    }
+}
