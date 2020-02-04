8282b39d945885c38b20aa212a9d3a78f372ec49
sjhkr2009 <49022232+sjhkr2009@users.noreply.github.com>
2020-02-04 14:03:02 +0900

    -

---
+++ StarPlanet/Assets/2.Scripts/ItemManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class ItemManager : MonoBehaviour
+{
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
