7e7b7efd0ba960c1bab93eef3f1c64fb0cba5e94
sjhkr2009 <49022232+sjhkr2009@users.noreply.github.com>
2020-05-26 23:07:10 +0900

    dp

---
+++ Design Pattern/Assets/07.Abstract Factory 2/AF_Factories.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AF_Race
+{
+    Terran,
+    Zerg,
+    Protoss
+}
+
+public abstract class RaceFactory
+{
+    public abstract 
+}
+
+public class TerranFactory :
