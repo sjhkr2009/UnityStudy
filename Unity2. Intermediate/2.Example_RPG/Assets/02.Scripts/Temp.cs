d379f3ca6f1afe30fcbb00d2074568a46b62add7
sjhkr2009 <49022232+sjhkr2009@users.noreply.github.com>
2020-08-13 12:51:09 +0900

    -

---
+++ Unity2. Intermediate/2.Example_RPG/Assets/02.Scripts/Temp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{
+    
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log(gameObject.GetComponent<BoxCollider>());
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log(gameObject.AddComponent<BoxCollider>());
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Component component1 = GetComponent<BoxCollider>();
+            Destroy(component1);
+            Debug.Log(gameObject.GetComponent<BoxCollider>());
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Component component1 = GetComponent<BoxCollider>();
+            DestroyImmediate(component1);
+            Debug.Log(gameObject.GetComponent<BoxCollider>());
+        }
+
+    }
+}
