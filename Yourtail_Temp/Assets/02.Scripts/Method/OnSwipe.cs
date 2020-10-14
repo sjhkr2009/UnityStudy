c2dcad7cd570050a52d52cc31626f4a3ccb5b0c9
sjhkr2009 <49022232+sjhkr2009@users.noreply.github.com>
2020-10-14 20:02:06 +0900

    a0.76.1
    스크롤링 테스트

---
+++ Yourtail_Temp/Assets/02.Scripts/Method/OnSwipe.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnSwipe : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    Vector2 startPos;
+    Vector2 endPos;
+    float moveDist;
+    float moveTime;
+    float moveSpeed;
+
+    Vector2 prevPos;
+    int count;
+
+    public void OnPointerDown(PointerEventData p)
+    {
+        moveDist = 0f;
+        moveTime = 0f;
+
+        startPos = p.position;
+        prevPos = p.position;
+    }
+    public void OnDrag(PointerEventData p)
+    {
+        float delta = Vector2.Distance(prevPos, p.position);
+        //Debug.Log($"{count++}회차 거리 : {delta}");
+        prevPos = p.position;
+    }
+    public void OnPointerUp(PointerEventData p)
+    {
+        endPos = p.position;
+        moveDist = Vector2.Distance(startPos, endPos);
+        moveSpeed = moveDist / moveTime;
+        Debug.Log($"총 이동거리: {moveDist} / 이동 시간: {moveTime} / 이동 속도: {moveSpeed}");
+    }
+
+    private void Update()
+    {
+        moveTime += Time.deltaTime;
+    }
+}
