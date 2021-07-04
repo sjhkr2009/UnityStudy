3ba2558ee83334f8299281aabdec7a43de5c1966
태사단 <49022232+sjhkr2009@users.noreply.github.com>
2021-07-04 17:58:24 +0900

    test dotween path

---
+++ Unity2. Intermediate/5.MMORPG/Client/Assets/Scripts/TestDotween.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class TestDotween : MonoBehaviour
+{
+    private PathType pathtype = PathType.CubicBezier;
+    [SerializeField] private DOTweenPath dotpath;
+    void Start()
+    {
+        dotpath = GetComponent<DOTweenPath>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Key A Pressed");
+            if(DOTween.IsTweening(transform, true)) transform.DOKill();
+            transform.DOPath(dotpath.path, 5f);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Key S Pressed");
+            transform.DOKill();
+            transform.DOPath(new Vector3[]
+            {
+                new Vector3(0, 0, 0),
+                new Vector3(3, 0, 0),
+                new Vector3(0, 3, 0)
+            }, 5f);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Key D Pressed");
+            transform.DOKill();
+            transform.DOPath(new Vector3[]
+            {
+                new Vector3(0, 0, 0),
+                new Vector3(3, 0, 0),
+                new Vector3(0, 3, 0)
+            }, 5f, PathType.CubicBezier);
+        }
+    }
+}
