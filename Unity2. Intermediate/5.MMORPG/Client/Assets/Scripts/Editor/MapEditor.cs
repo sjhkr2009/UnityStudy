cef94b06c2e9b82066826d787522e81a29387f83
태사단 <49022232+sjhkr2009@users.noreply.github.com>
2021-07-09 00:04:28 +0900

    2-5-Add MapInfo Generator (Editor)

---
+++ Unity2. Intermediate/5.MMORPG/Client/Assets/Scripts/Editor/MapEditor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEngine.Tilemaps;
+
+#endif
+
+public class MapEditor
+{
+#if UNITY_EDITOR
+    // 단축키는 MenuItem의 마지막에 띄어쓰기 후 붙일 수 있으며, 컨트롤(command)/알트(option)/쉬프트가 각각 %, #, & 로 표현된다.
+    [MenuItem("Custom Tools/Hello World %#&h")]
+    private static void HelloWorld() {
+        if (EditorUtility.DisplayDialog("Hello World", "Create?", "OK", "No")) {
+            new GameObject("Hello World");
+        }
+    }
+    
+    [MenuItem("Custom Tools/Generate Map %m")]
+    private static void GenerateMapInfo() {
+        GameObject[] maps = Resources.LoadAll<GameObject>("Prefabs/Map");
+
+        foreach (var map in maps) {
+            Tilemap tilemap = CustomUtility.FindChild<Tilemap>(map, "Tilemap_Collision", true);
+            WriteMapInfo(tilemap);
+        }
+    }
+
+    static void WriteMapInfo(Tilemap tilemap) {
+        using (var writer = File.CreateText("Assets/Resources/Map/mapinfo.txt")) {
+            writer.WriteLine(tilemap.cellBounds.xMin);
+            writer.WriteLine(tilemap.cellBounds.xMax);
+            writer.WriteLine(tilemap.cellBounds.yMin);
+            writer.WriteLine(tilemap.cellBounds.yMax);
+
+            for (int y = tilemap.cellBounds.yMax; y >= tilemap.cellBounds.yMin; --y) {
+                for (int x = tilemap.cellBounds.xMin; x < tilemap.cellBounds.xMax; ++x) {
+                    var tile = tilemap.GetTile(new Vector3Int(x, y, 0));
+                    if(tile != null)
+                        writer.Write("1");
+                    else
+                        writer.Write("0");
+                }
+                writer.WriteLine();
+            }
+        }
+    }
+    
+#endif
+}
+
---
+++ Unity2. Intermediate/5.MMORPG/Client/Assets/Scripts/TestCollision.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TestCollision : MonoBehaviour
+{
+    [SerializeField] private Tilemap tilemap;
+    [SerializeField] private TileBase tile;
+    
+    List<Vector3Int> blocks = new List<Vector3Int>();
+    private void Start()
+    {
+        // 타일맵의 경계 내 모든 좌표값을 탐색한다.
+        foreach (Vector3Int pos in tilemap.cellBounds.allPositionsWithin)
+        {
+            //타일을 가져와서, 빈 칸이 아니면(타일이 있으면) 해당 위치를 blocked에 저장한다.
+            TileBase tileBase = tilemap.GetTile(pos);
+            if(tileBase != null) blocks.Add(pos);
+            //반대로 SetTile로 동적으로 타일맵에 타일을 배치할 수도 있다.
+            //tilemap.SetTile(pos, tile);
+        }
+
+        foreach (var block in blocks)
+        {
+            Debug.Log(block);
+        }
+    }
+}
