d287c128f2bd3c502d83c9642ca42ad49c04e295
sjhkr2009 <sjhkr2009@naver.com>
2022-11-06 11:10:37 +0900

    Editor Project - 일부 텍스트 변환/미리보기

--- Editor Project/Assets/Custom/Editor/EventManagerColorEditor.cs
+++ Editor Project/Assets/Custom/Editor/EventManagerColorEditor.cs
@@ -16,39 +16,50 @@ public class EventManagerColorEditor : Editor {
     const int ColorPrefixLength = 15; // "<color=#000000>".Length
     const int ColorPostfixLength = 8; // "</color>.Length
     const int ColorTotalLength = ColorPrefixLength + ColorPostfixLength;
-    
+
+    private static string selectedText = string.Empty;
+
     public override void OnInspectorGUI() {
         base.OnInspectorGUI();
-        
+
+        if (EditorGUIUtility.textFieldHasSelection) {
+            // 얘는 GUILayout.TextArea 에서만 동작하고 EditorGUI 기반의 에디터에선 동작하지 않아서, 리플렉션을 사용하기로 함
+            //TextEditor editor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
+            TextEditor editor = typeof(EditorGUI).GetField("activeEditor", BindingFlags.Static | BindingFlags.NonPublic)
+                ?.GetValue(null) as TextEditor;
+            if (!string.IsNullOrEmpty(editor?.SelectedText)) {
+                selectedText = editor.SelectedText;
+            }
+        }
+
         EditorGUILayout.LabelField("색상");
         selectedColor = EditorGUILayout.ColorField(selectedColor);
+        EditorGUILayout.LabelField($"Selected: {selectedText}");
         if (GUILayout.Button("적용하기")) {
-            GUIUtility.systemCopyBuffer = $"<color=#{ColorUtility.ToHtmlStringRGB(selectedColor)}></color>";
-            if (TryToColoredString(inputText, out var content, out _)) {
-                inputText = $"<color=#{ColorUtility.ToHtmlStringRGB(selectedColor)}>{content}</color>";
-            } else {
-                inputText = $"<color=#{ColorUtility.ToHtmlStringRGB(selectedColor)}>{inputText}</color>";
+            var startPos = inputText.IndexOf(selectedText, StringComparison.InvariantCulture);
+            if (startPos >= 0) {
+                Regex prevColorRegex = new Regex(@"(<color=#[0-9a-fA-F]{6}>)" + selectedText + "(<[/]color>)");
+                var matchInfo = prevColorRegex.Match(inputText);
+                if (matchInfo.Success && matchInfo.Value.Contains(selectedText)) {
+                    inputText = prevColorRegex.Replace(inputText, $"<color=#{ColorUtility.ToHtmlStringRGB(selectedColor)}>{selectedText}</color>", 1);
+                } else {
+                    var prefix = inputText.Substring(0, startPos);
+                    var targetText = $"<color=#{ColorUtility.ToHtmlStringRGB(selectedColor)}>{selectedText}</color>";
+                    var postfix = inputText.Substring(startPos + selectedText.Length);
+                    inputText = prefix + targetText + postfix;
+                }
             }
+
+            selectedText = string.Empty;
         }
+
+        var richTextStyle = new GUIStyle(EditorStyles.boldLabel) {
+            richText = true
+        };
+
         EditorGUILayout.LabelField("텍스트");
         inputText = EditorGUILayout.TextArea(inputText);
-        
-        GUI.color = Color.green;
-        if (TryToColoredString(inputText, out var inputTextRich, out var textColor)) {
-            EditorGUILayout.LabelField($"Color Code: {ColorUtility.ToHtmlStringRGB(textColor)}");
-            GUI.color = textColor;
-            EditorGUILayout.TextArea($"Text: {inputTextRich}");
-        } else {
-            if (EditorGUIUtility.textFieldHasSelection) {
-                // 얘는 GUILayout.TextArea 에서만 동작하고 EditorGUI 기반의 에디터에선 동작하지 않아서, 리플렉션을 사용하기로 함
-                //TextEditor editor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
-                TextEditor editor = typeof(EditorGUI)
-                    .GetField("activeEditor", BindingFlags.Static | BindingFlags.NonPublic)?.GetValue(null) as TextEditor;
-                if(!string.IsNullOrEmpty(editor?.SelectedText)) EditorGUILayout.LabelField($"Selected: {editor.SelectedText}");
-            }
-            EditorGUILayout.LabelField($"Is Not Colored Text");
-        }
-        GUI.color = Color.white;
+        EditorGUILayout.LabelField(inputText, richTextStyle);
     }
 
     bool TryToColoredString(string htmlColoredString, out string content, out Color color) {
